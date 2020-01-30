5ca3741698352a3d0e78618bf9543c95a5a4c6db
Jeff Huang <jeffshuang2@gmail.com>
2020-01-29 18:35:55 -0800

    Removed the included CSS and images from create-react-app. Started on the header component.

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-dark" >
+                    <div className="container">
+                   <h1>React Clicky Game</h1>
+                   <h1>Click an image to begin!</h1>
+                   <h1>Score goes here</h1>
+                   </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header 
