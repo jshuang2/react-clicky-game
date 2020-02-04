cf13baff13e0ccfb063f38f4b2eea2d2ca6dfa3e
Jeff Huang <jeffshuang2@gmail.com>
2020-02-03 20:28:47 -0800

    Fixed bug where handleImageChoice was not being called properly.

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -4,19 +4,16 @@ import Card from "../Card/Card";
 
 class CardList extends Component {
 
+        state = {
+            images,
+            chosenImages: new Set()
+          }
     
 
-    state = {
-        images,
-        chosenImages: new Set()
-      }
-
-    // Function to listen for chosen image. If the image has not yet been selected, push it to a set called ChosenCards. If it has, then call the reset function
-    
-    
-    
+    // Function to listen for chosen image. If the image has not yet been selected, push it to a set called ChosenCards. If it has, then call the reset function    
     handleImageChoice = event => {
         
+        console.log("this is state...." + JSON.stringify(this.state.images))
         let chosenID = event.target.id;
 
         if (!this.state.chosenImages.has(chosenID)) {
@@ -34,7 +31,6 @@ class CardList extends Component {
     };
     
     // Function to randomize and shuffle the images array after each chosen card
-    
     shuffle = (imagesArray) => {
         for (let i = imagesArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -42,41 +38,39 @@ class CardList extends Component {
         }
 
         console.log(imagesArray);
-    }
+        return imagesArray;
+    };
+
+
     // Function to reset the game. This will be called in another function.
-
-
-
     resetGame = () => {
-        // this.shuffle(this.state.images);
+        this.shuffle(this.state.images);
 
         this.setState({
-            images: this.shuffle(this.state.images),
+            images,
             chosenImages: new Set()
         })
-    }
 
-
-
-
-
+        alert("YOU LOST");
+    };
 
 
 
     render() {
         console.log(this.state.images);
         
-        this.handleImageChoice();
         
         
         return (
             
             this.state.images.map(image => (
+            <div key={image.id} onClick={this.handleImageChoice}>
             <Card 
                 key={image.id} 
                 src={image.image}
                 alt={image.name}
             />
+            </div>
             ))
         )
     }
