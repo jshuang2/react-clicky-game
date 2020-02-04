c7ccf435b6c253e2c0b52c79b360d551bcbfc567
Jeff Huang <jeffshuang2@gmail.com>
2020-02-03 17:58:01 -0800

    Created function to handle user input for image choices.

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -4,16 +4,64 @@ import Card from "../Card/Card";
 
 class CardList extends Component {
 
+    
+
     state = {
-        images
+        images,
+        chosenImages: new Set()
       }
+
+    // Function to listen for chosen image. If the image has not yet been selected, push it to a set called ChosenCards. If it has, then call the reset function
+    
+    
+    
+    handleImageChoice = event => {
+        
+        let chosenID = event.target.id;
+
+        if (!this.state.chosenImages.has(chosenID)) {
+            this.shuffle(this.state.images);
+
+            this.setState(({ chosenImages }) => ({
+                images,
+                chosenImages: new Set(chosenImages).add(chosenID)
+              }));
+        }
+        else {
+            this.resetGame();
+        };
+        
+    };
+    
+    // Function to randomize and shuffle the images array after each chosen card
+    
+    shuffle = () => {
+
+    }
+    // Function to reset the game. This will be called in another function.
+
+
+
+    resetGame = () => {
+
+    }
+
+
+
+
+
+
+
 
     render() {
         console.log(this.state.images);
         
+        this.handleImageChoice();
+        
+        
         return (
             
-            this.state.images.map((image) => (
+            this.state.images.map(image => (
             <Card 
                 key={image.id} 
                 src={image.image}
